9265359489e27dc3b48622d8f1ad408ecb8d5268
haonam2090193 <thcsthg.haonam@gmail.com>
2023-07-29 16:53:59 +0700

    [add] LDSP

--- Assets/MyGame/Scripts/Pooling/ObjectPool.cs
+++ Assets/MyGame/Scripts/Pooling/ObjectPool.cs
@@ -13,17 +13,8 @@ public class ObjectPool : BaseManager<ObjectPool>
     private void Start()
     {
         pooledObjects = new List<Bullet>();
-        pooledEnemyObjects = new List<Enemy>();
         amountToPool = DataManager.Instance.GlobalConfig.maxBulletPoolSize;
-        amountToPoolE = DataManager.Instance.GlobalConfig.maxEnemyPoolSize;
         Bullet tmp;
-        Enemy enemy;
-        for (int i = 0; i < amountToPoolE; i++)
-        {
-            enemy = Instantiate(objectToPoolE, this.transform,true);
-            enemy.Deactive();
-            pooledEnemyObjects.Add(enemy);
-        }
         for (int i = 0; i < amountToPool; i++)
         {
             tmp = Instantiate(objectToPool,this.transform,true);
@@ -39,17 +30,6 @@ public class ObjectPool : BaseManager<ObjectPool>
             if (!pooledObjects[i].IsActive)
             {
                 return pooledObjects[i];
-            }
-        }
-        return null;
-    }
-    public Enemy GetPooledObjectE()
-    {
-        for (int i = 0; i < amountToPoolE; i++)
-        {
-            if (!pooledEnemyObjects[i].IsActive)
-            {
-                return pooledEnemyObjects[i];
             }
         }
         return null;
