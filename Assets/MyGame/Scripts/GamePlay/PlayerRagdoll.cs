9b22a213d645bfb78e5ec48bf3d61a2802303a56
MAY01\May1 <thcsthg.haonam@gmail.com>
2023-08-17 18:33:39 +0700

    [update] ragdoll

---
+++ Assets/MyGame/Scripts/GamePlay/PlayerRagdoll.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerRagdoll : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody[] rigidBodies;
+    private CharacterController characterController;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidBodies = GetComponentsInChildren<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+
+        // DeactiveRagdoll();
+        ActiveRagdoll();
+
+
+    }
+
+    public void DeactiveRagdoll()
+    {
+        foreach (var rigidBody in rigidBodies)
+        {
+            rigidBody.isKinematic = true;
+        }
+
+        animator.enabled = true;
+        characterController.enabled = true;
+
+    }
+
+    public void ActiveRagdoll()
+    {
+        foreach (var rigidBody in rigidBodies)
+        {
+            rigidBody.isKinematic = false;
+        }
+        characterController.enabled = false;
+        animator.enabled = false;
+    }
+    public void ApplyForce(Vector3 force, Rigidbody rigidbody)
+    {
+        rigidbody.AddForce(force, ForceMode.VelocityChange);
+    }
+}
