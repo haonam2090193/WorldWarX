7ab59c2b14a0c47a83bef662d8ccc18860639a9a
haonam2090193 <thcsthg.haonam@gmail.com>
2023-08-21 21:55:18 +0700

    [Update] UI

--- Assets/ButtonTest.cs
+++ Assets/ButtonTest.cs
@@ -5,13 +5,44 @@ using UnityEngine.UI;
 
 public class ButtonTest : MonoBehaviour
 {
+    private CanvasGroup canvasGroups;
+    private Toggle toggle;
 
-    public void CloseSettingButton()
+    private void Awake()
     {
-        GameObject.Find("SettingPanel").SetActive(false);
+        toggle = GetComponentInChildren<Toggle>();
+        canvasGroups = GetComponentInChildren<CanvasGroup>();
     }
-    public void OpenSettingButton()
+    private void Start()
     {
-        GameObject.Find("SettingPanel").SetActive(true);
+        DeactivePanel();
+       // DeactiveToggle();
     }
+    public void ActivePanel()
+    {
+        Debug.Log("Active");
+        canvasGroups.alpha = 1;
+        canvasGroups.blocksRaycasts = true;
+        canvasGroups.interactable = true;
+    }
+    public void DeactivePanel()
+    {
+
+        Debug.Log("Deactive");
+
+        canvasGroups.alpha = 0;
+        canvasGroups.blocksRaycasts = false;
+        canvasGroups.interactable = false;
+    }
+    
+    public void ActiveToggle()
+    { 
+        toggle.isOn = true;
+        Debug.Log("Active Toggle");
+    }
+    public void DeactiveToggle()
+    {
+        toggle.isOn = false;
+        Debug.Log("Deactive Toggle");
+    } 
 }
