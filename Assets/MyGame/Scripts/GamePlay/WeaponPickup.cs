5480988bf9e912acf34e288c5ae89512efcbd2f7
MAY16\MAY23 <thcsthg.haonam@gmail.com>
2023-08-28 16:50:30 +0700

    [update] Pick up Weapon

--- Assets/MyGame/Scripts/GamePlay/WeaponPickup.cs
+++ Assets/MyGame/Scripts/GamePlay/WeaponPickup.cs
@@ -7,12 +7,12 @@ public class WeaponPickup : MonoBehaviour
     public RaycastWeapon weaponPrefab;
 
     private void OnTriggerEnter(Collider other)
-    {
-        ActiveWeapon activeWeapon = other.gameObject.GetComponent<ActiveWeapon>();
-        if (activeWeapon)
-        {
-            RaycastWeapon newWeapon = Instantiate(weaponPrefab);
-            activeWeapon.Equip(newWeapon);
-        }
-    }
+    {  
+            ActiveWeapon activeWeapon = other.gameObject.GetComponent<ActiveWeapon>();
+            if (activeWeapon)
+            {
+                RaycastWeapon newWeapon = Instantiate(weaponPrefab);
+                activeWeapon.Equip(newWeapon);
+            }      
+    }   
 }
