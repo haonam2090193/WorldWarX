6022fd3da905fb6657e1e4f5ea8c7337783c4027
MAY16\MAY23 <thcsthg.haonam@gmail.com>
2023-08-28 17:03:49 +0700

    no message

---
+++ Assets/MyGame/Scripts/Data/WeaponData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+
+public class WeaponData : ScriptableObject
+{
+    [Header("Stats")]
+    public float fireRate = 25;
+    public float bulletSpeed = 1000f;
+    public float bulletDrop = 0f;
+    public int ammoCount;
+    public int totalAmmo;
+    public float damage;
+    public int ammoPerShot;
+    public bool singleMode;
+    public float[] spreads;
+}
