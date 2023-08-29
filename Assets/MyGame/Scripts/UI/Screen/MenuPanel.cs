bffa79c032cefb1382b3ad1a2cd925cd5dcd10fc
MAY16\MAY23 <thcsthg.haonam@gmail.com>
2023-08-29 14:52:12 +0700

    [update] Pickup Weapon

---
+++ Assets/MyGame/Scripts/UI/Screen/MenuPanel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuPanel : BaseScreen
+{
+    public override void Show(object data)
+    {
+        base.Show(data);
+    }
+    
+    public override void Hide()
+    {
+        base.Hide();
+    }   
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Map1");
+        this.Hide();
+        Debug.Log("Loaded");
+    }
+    
+    public void OnClickPopupSetting()
+    {
+        if (UIManager.HasInstance)
+        {
+            UIManager.Instance.ShowPopup<PopupSetting>();
+        }
+    }
+        
+}
