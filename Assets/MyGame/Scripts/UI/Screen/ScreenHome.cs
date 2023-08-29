a58efdf5be98bc28cdce99ee56187fe41c3f4978
MAY01\May1 <thcsthg.haonam@gmail.com>
2023-08-29 19:38:49 +0700

    [fix] animation, UI

--- Assets/MyGame/Scripts/UI/Screen/ScreenHome.cs
+++ Assets/MyGame/Scripts/UI/Screen/ScreenHome.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-public class MenuPanel : BaseScreen
+public class ScreenHome : BaseScreen
 {
     public override void Show(object data)
     {
