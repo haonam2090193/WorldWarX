96c4bc55c70b00ca68ae378fbd5f334127b518d1
haonam2090193 <thcsthg.haonam@gmail.com>
2023-09-02 20:50:32 +0700

    [update] UI

---
+++ Assets/MyGame/Scripts/UI/Screen/InGameMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenu : BasePopup
+{
+    public override void Hide()
+    {
+        base.Hide();
+    }
+
+    public override void Show(object data)
+    {
+        base.Show(data);
+    }
+}
